53bc7c7c36b105dbd2cd52d676155614f5a140ff
gabrielg21 <gabo21gonzalez@gmail.com>
2017-11-14 16:09:08 -0600

    Add files via upload

---
+++ configuracionCuenta.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estiloperfil.css">
+
+	<title>Pinterest • El catálogo global de ideas</title>
+	<link rel="icon" href="img/favicon" sizes="16x16" type="image/jpg">
+</head>
+<body>
+	<div>
+		<a href="pinterest.html"><button type="button" class=" btn btn-default cambiacolor"  style="margin-left: 20px; font-weight:bold; ">
+	   	 <img src="img/logo.png" height="34" width="34"><br><p class="tamaño"></p>
+	   </button></a>
+	
+	
+		<input type="text" name="search" id=search placeholder="Buscar" class="input">
+	 
+	
+		<button type="button" class="btn btn-default cambiacolor" >
+			<i style="color:#969696" class="fa fa-navicon fa-2x "><br><p style="font-family: helvetica; font-weight:bold; " class="tamaño">Inicio</p></i>
+		</button>
+	
+	
+		<button type="button" class="btn btn-default cambiacolor" >
+			<i style="color:#969696" class="fa fa-user fa-2x "><br><div style="height: 24px; width: 24px;"><div style="background-color: #EFEFEF; padding-bottom: 100%;"><img src="img/trunks.jpg" alt="trunks" class="img-responsive img-circle"></div></div><p style="font-family: helvetica;font-weight: bold;" class="tamaño">Nombre Usuario</p></i>
+		</button>
+	
+	
+		<button type="button" class="btn btn-default cambiacolor" >
+			<i style="color:#969696" class="fa fa-commenting fa-2x "><br><p style="font-family: helvetica;font-weight: bold;" class="tamaño"><span class="glyphicon glyphicon-menu-hamburger"></span></p></i>
+		</button>
+
+		<button type="button" class="btn btn-default cambiacolor" >
+			<i style="color:#969696" class="fa fa-commenting fa-2x "><br><p style="font-family: helvetica;font-weight: bold;" class="tamaño"><span class="glyphicon glyphicon-bell"></span></p></i>
+		</button>
+
+		<button type="button" class="btn btn-default cambiacolor" >
+			<i style="color:#969696" class="fa fa-commenting fa-2x "><br><p style="font-family: helvetica;font-weight: bold;" class="tamaño"><span class="glyphicon glyphicon-option-horizontal"></span></p></i>
+		</button>
+
+		<br>
+		<hr style="margin-top: -2px">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6" style="margin-left:80px; margin-top: 20px;">
+					<div>
+						<p style="color: rgb(85,85, 85); cursor:auto; display:block; font-family: helvetica ; font-size:18px; font-style:normal; font-weight:bold; height:auto;hyphens:manual; letter-spacing: -0.25px; line-height:20px; padding-bottom:4px; padding-left: 4px; padding-right:4px; padding-top:4px;text-align: left; text-size-adjust: 100%;">Aspectos básicos de la cuenta</p>
+						<p>Perfil</p>
+						<p>Notificaciones</p>
+						<p>Feed de Inicio</p>
+						<p>Seguridad</p>
+					</div>
+				</div>
+			</div>	
+		</div>	
+		<div class="container">
+			<div class="row">			
+					<div class="col-lg-6">
+						<h2>Apectos básicos de la cuenta</h2>
+						<p>Correo</p>
+						<input type="TEXT" name="" class="btn-default form-control">
+						<p style="gb(85, 85, 85); display: block; font-family: Helvetica;font-size: 16px; font-style: normal; font-weight :normal;height: 18px; hyphens: manual; letter-spacing: -0.2px; line-height: 18px;text-align: left; text-size-adjust: 100%;">Ccontraseña</p>
+
+						<input type="TEXT" name="" class="btn-default form-control" value="Cambiar Contraseña">
+						<p>Idioma</p>
+						<select class="form-control">
+							<option>Español (Américas)</option>
+							<option>Español (España)</option>
+							<option>Caliche x) </option>
+						</select>
+						<p>Género</p>
+						<label><input type="radio" name="">Hombre</label>
+						<label><input type="radio" name="">Mujer</label>
+						<label><input type="radio" name="">Personalizado</label>
+						<p>Privacidad de búsqueda</p>
+						<label><input type="checkbox" name="" value="on">Ocultar tu perfil de los motores de búsqueda (p. ej. google).</label>
+						<p>Personalización</p>
+						<label><input type="checkbox" name="">Usa los sitios que visitas para mejorar las recomendaciones y anuncion que se mostrarán <a href="">Más información</a></label>
+						<label><input type="checkbox" name="">Utiliza la información de nuestros socios para mejorar las recomendadiones y los anuncios que se te mostrarán <a href="">Más información</a></label>
+						<p>Historial de búsqueda</p>
+						<label><input type="button" name="" class="btn btn-default cambiacolor" value="Borrar búsquedas recientes">Eliminar de las sugerencias de búsqueda los elementos buscados recientemente</label>
+						<p>Cuenta</p>
+						<input type="button" name="" class="btn-default btn cambiacolor" value="Desactivar cuenta">
+						<h2>Perfil</h2>
+						<table cellspacing="10px" class="table-hover">
+							<tr>
+								<td>Nombre</td>
+								<td>Apellido</td>
+							</tr>
+							<tr>
+								<td><input type="text" name="" class="btn-default form-control" value="Trunks"></td>
+								<td><input type="text" name="" class="btn-default form-control" value="Sayajin"></td>
+							</tr>
+						</table>
+						<p>Foto</p>
+						<div style="height: 40px; width: 40px; padding-right: 10px;"></div><img src="img/trunks.jpg" alt="" class="img-responsive img-circle" ><input type="button" name="" class="btn btn-default cambiacolor" value="Cambiar Imagen">
+						<p>Nombre de usuario</p>
+						<span>www.pinterest.com/</span> 
+						<input type="text" name="" value="TrunksFutureWarrior" class="btn-default btn-default">
+						<p>Acerca de ti</p>
+						<textarea class="btn btn-default cambiacolor"></textarea>
+						<p>Ubicación</p>
+						<input type="text" name="">
+						<p>Sitio web</p>
+						<input type="text" name="">
+						<input type="button" name="" value="confirmar sitio web">
+						<h2>Notificaciones</h2>
+						<p>Siempre te informaremos sobre los cambios importantes, pero tú debes elegir sobre qué más quieres saber</p>
+						<div>
+							
+						</div>
+						<div></div>
+						<div></div>
+						<h2>Feed de Inicio</h2>
+						<p>Elegido para ti</p>
+						<label><input type="checkbox" name="">Mostrar pines "Elegidos para ti" en tu feed de inicio <a href="">Más información</a></label>
+						<h2>Seguridad</h2>
+						<p>Esta es la lista de dispositivos que han iniciado sesión en tu cuenta.</p>
+						<p>Anula las sesiones que no reconozcas</p>
+						<input type="button" name=""  class="btn" value="Mostrar sesiones">
+					</div>
+					<!--<div data-test-login="true" style="background-color:#fff;border-radius:8px;position:relative;text-align:center;width:404px;margin:auto;min-height:520px;box-shadow:0 2px 10px rgba(0,0,0,0.45);"></div>-->
+			</div>	
+		</div>	
+<!--<a href="iniciarSesion.html"> Iniciar Sesion</a>-->
+
+
+	<script type="text/javascript" src="js/funcionalidades-login.js"></script>
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
