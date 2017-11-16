15fc8d3306fdebad2fa1f955240224418b75bbe7
gabrielg21 <gabo21gonzalez@gmail.com>
2017-11-15 22:05:39 -0600

    Encriptación de la contraseña
    se encripta la contraseña al verificar si el usuario existe y se compara así con la contraseña encriptada en la BD

--- ajax/acciones-InicioSesion.php
+++ ajax/acciones-InicioSesion.php
@@ -6,7 +6,8 @@
 			include_once("../class/class-conexion.php");
 			include_once("../class/class-usuario.php");
 			$conexion = new Conexion();
-			Usuario::verificarUsuario($conexion,$_POST["contrasenna"],$_POST["div-telefono-contrasena"]);
+			$contrasena = sha1($_POST["contrasenna"]);
+			Usuario::verificarUsuario($conexion,$contrasena,$_POST["div-telefono-contrasena"]);
 			break;
 	default:
 			
